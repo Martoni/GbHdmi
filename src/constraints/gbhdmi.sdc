b6ece9ff0132c90ed040e74005518750de8807ad
Fabien Marteau <mail@fabienm.eu>
2022-03-26 20:44:15 +0100

    change pad controls wiring (tobefixed)

--- src/constraints/gbhdmi.sdc
+++ src/constraints/gbhdmi.sdc
@@ -4,5 +4,5 @@
 //GOWIN Version: 1.9.7.02 Beta
 //Created Time: 2021-06-01 10:34:02
 create_clock -name I_clk -period 37.037 -waveform {0 18.518} [get_ports {I_clk}] -add
-create_clock -name serial_clk -period 2.694 -waveform {0 1.347} [get_nets {tmdsPllvr_clkout}] -add
+create_clock -name serial_clk -period 2.694 -waveform {0 1.347} [get_nets {tmdsPllvr_io_clkout}] -add
 create_clock -name pix_clk -period 13.468 -waveform {0 6.734} [get_nets {clkDiv_CLKOUT}] -add
--- src/main/scala/topgbhdmi.scala
+++ src/main/scala/topgbhdmi.scala
@@ -34,19 +34,22 @@ class TopGbHdmi(gowinDviTx: Boolean = true) extends RawModule {
     val snes_dclock = IO(Output(Bool()))
     val snes_dlatch = IO(Output(Bool()))
     val snes_sdata = IO(Input(Bool()))
-    
+
     /* gb pad output */
-    val gbpad_a      = IO(Output(Bool()))
-    val gbpad_b      = IO(Output(Bool()))
-    val gbpad_select = IO(Output(Bool()))
-    val gbpad_start  = IO(Output(Bool()))
+    val P10= IO(Output(Bool())) //gbpad_a and right
+    val P11= IO(Output(Bool())) //gbpad_b and left
+    val P12= IO(Output(Bool())) //gbpad_select and up
+    val P13= IO(Output(Bool())) //gbpad_start and down
     val gbpad_right  = IO(Output(Bool()))
     val gbpad_left   = IO(Output(Bool()))
-    val gbpad_up     = IO(Output(Bool()))
-    val gbpad_down   = IO(Output(Bool()))
+    val P14 = IO(Input(Bool())) // direction pad
+    val P15 = IO(Input(Bool())) // buttons and config
     /********************************************/
 
-    O_led := 1.U(2.W) 
+    gbpad_right := true.B
+    gbpad_left := true.B
+
+    O_led := 0.U(2.W)
 
     val pll_lock =  Wire(Bool())
     val serial_clk = Wire(Clock())
@@ -69,63 +72,82 @@ class TopGbHdmi(gowinDviTx: Boolean = true) extends RawModule {
 
     withClockAndReset(pix_clk, glb_rst) {
 
-      /* plug SuperNes pad */
-      val cnpd = Module(new ChisNesPad(mainClockFreq, clockFreq, 16))
-      val sNesPadReg = RegInit(0.U(16.W))
-      /* input shift reg ctrl */
-      snes_dclock := cnpd.io.dclock
-      snes_dlatch := cnpd.io.dlatch
-      cnpd.io.sdata := snes_sdata
-      /* output register */
-      cnpd.io.data.ready := true.B
-      when(cnpd.io.data.valid){
-        sNesPadReg := ~cnpd.io.data.bits
-      }
-      gbpad_b      := sNesPadReg(15)
-      //pad_y      := sNesPadReg(14)
-      gbpad_select := sNesPadReg(13)
-      gbpad_start  := sNesPadReg(12)
-      gbpad_up     := sNesPadReg(11)
-      gbpad_down   := sNesPadReg(10)
-      gbpad_left   := sNesPadReg(9)
-      gbpad_right  := sNesPadReg(8)
-      gbpad_a      := sNesPadReg(7)
-      //pad_x      := sNesPadReg(6)
-      //pad_l      := sNesPadReg(5)
-      //pad_r      := sNesPadReg(4)
-      O_led := sNesPadReg(11) ## sNesPadReg(10)
+        /* plug SuperNes pad */
+        val cnpd = Module(new ChisNesPad(mainClockFreq, clockFreq, 16))
+        val sNesPadReg = RegInit(0.U(16.W))
+        /* input shift reg ctrl */
+        snes_dclock := cnpd.io.dclock
+        snes_dlatch := cnpd.io.dlatch
+        cnpd.io.sdata := snes_sdata
+        /* output register */
+        cnpd.io.data.ready := true.B
+        when(cnpd.io.data.valid){
+            sNesPadReg := cnpd.io.data.bits
+        }
 
-      /* synchronize gameboy input signals with clock */
-      val shsync = ShiftRegister(gb.hsync,2)
-      val svsync = ShiftRegister(gb.vsync,2)
-      val sclk   = ShiftRegister(gb.clk  ,2)
-      val sdata  = ShiftRegister(gb.data ,2)
+        //gbpad_b      := sNesPadReg(15)
+        ////pad_y      := sNesPadReg(14)
+        //gbpad_select := sNesPadReg(13)
+        //gbpad_start  := sNesPadReg(12)
+        //gbpad_up     := sNesPadReg(11)
+        //gbpad_down   := sNesPadReg(10)
+        //gbpad_left   := sNesPadReg(9)
+        //gbpad_right  := sNesPadReg(8)
+        //gbpad_a      := sNesPadReg(7)
+        ////pad_x      := sNesPadReg(6)
+        ////pad_l      := sNesPadReg(5)
+        ////pad_r      := sNesPadReg(4)
+        O_led := sNesPadReg(11) ## sNesPadReg(10)
 
-      /* top GbVga module instantiation */
-      val gbHdmi = Module(new GbHdmi())
+        /* Drive gameboy buttons */
 
-      gbHdmi.io.pattern_trig := sNesPadReg(4) | sNesPadReg(5)
+        P10 := true.B
+        P11 := true.B
+        P12 := true.B
+        P13 := true.B
+        when(ShiftRegister(P14, 2) === false.B){
+            P10 := sNesPadReg(8) // right
+            P11 := sNesPadReg(9) // left
+            P12 := sNesPadReg(11)// up
+            P13 := sNesPadReg(10)// down
+        }.elsewhen(ShiftRegister(P15, 2) === false.B){
+            P10 := sNesPadReg(7)  // a
+            P11 := sNesPadReg(15) // b
+            P12 := sNesPadReg(13) // select
+            P13 := sNesPadReg(12) // start
+        }
 
-      gbHdmi.io.gb.hsync := shsync
-      gbHdmi.io.gb.vsync := svsync
-      gbHdmi.io.gb.clk   := sclk
-      gbHdmi.io.gb.data  := sdata
+        /* synchronize gameboy input signals with clock */
+        val shsync = ShiftRegister(gb.hsync,2)
+        val svsync = ShiftRegister(gb.vsync,2)
+        val sclk   = ShiftRegister(gb.clk  ,2)
+        val sdata  = ShiftRegister(gb.data ,2)
 
-      gbHdmi.io.serClk := serial_clk
+        /* top GbVga module instantiation */
+        val gbHdmi = Module(new GbHdmi())
 
-      O_tmds.clk.p :=  gbHdmi.io.tmds.clk.p
-      O_tmds.clk.n  := gbHdmi.io.tmds.clk.n
-      O_tmds.data(0).p := gbHdmi.io.tmds.data(0).p
-      O_tmds.data(0).n := gbHdmi.io.tmds.data(0).n
-      O_tmds.data(1).p := gbHdmi.io.tmds.data(1).p
-      O_tmds.data(1).n := gbHdmi.io.tmds.data(1).n
-      O_tmds.data(2).p := gbHdmi.io.tmds.data(2).p
-      O_tmds.data(2).n := gbHdmi.io.tmds.data(2).n
+        gbHdmi.io.pattern_trig := sNesPadReg(4) | sNesPadReg(5)
+
+        gbHdmi.io.gb.hsync := shsync
+        gbHdmi.io.gb.vsync := svsync
+        gbHdmi.io.gb.clk   := sclk
+        gbHdmi.io.gb.data  := sdata
+
+        gbHdmi.io.serClk := serial_clk
+
+        O_tmds.clk.p :=  gbHdmi.io.tmds.clk.p
+        O_tmds.clk.n  := gbHdmi.io.tmds.clk.n
+        O_tmds.data(0).p := gbHdmi.io.tmds.data(0).p
+        O_tmds.data(0).n := gbHdmi.io.tmds.data(0).n
+        O_tmds.data(1).p := gbHdmi.io.tmds.data(1).p
+        O_tmds.data(1).n := gbHdmi.io.tmds.data(1).n
+        O_tmds.data(2).p := gbHdmi.io.tmds.data(2).p
+        O_tmds.data(2).n := gbHdmi.io.tmds.data(2).n
     }
 }
 
 object TopGbHdmiDriver extends App {
-  println("Generate GbHdmi with open source HdmiCore core")
-  (new ChiselStage).execute(args,
-    Seq(ChiselGeneratorAnnotation(() => new TopGbHdmi())))
+    println("Generate GbHdmi with open source HdmiCore core")
+    (new ChiselStage).execute(args,
+        Seq(ChiselGeneratorAnnotation(() => new TopGbHdmi())))
 }
